2474832542a9b617f37ef9c53e1f5e614c519ede
Yelsin fabian <yelsincito@mail.com>
2020-01-26 08:57:04 -0500

    cambios

--- app.js
+++ app.js
@@ -11,13 +11,18 @@ app.use('/public', express.static(path.join(__dirname,('./public'))));
 
 const homeRuta = require('./rutas/homeRuta');
 const menuRuta = require('./rutas/menuRuta');
+const nosotrosRuta = require('./rutas/nosotrosRuta');
+const contactosRuta = require('./rutas/contactosRuta');
+
 
 app.get('/', function(req, res, next){
     res.render('index.pug',{});
 });
-
 app.use('/home', homeRuta);
 app.use('/menu', menuRuta);
+app.use('/nosotros', nosotrosRuta);
+app.use('/contactos', contactosRuta);
+
 
 app.listen(3000, function(){
     console.log('servidor inciando');
---
+++ rutas/contactosRuta.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const path = require('path');
+const app = express();
+
+router.get('/', async function (req, res, next) {
+    try {
+        
+        res.render('contactos.pug');
+    } catch(err) {
+        res.status(500).json({error: err});
+    }
+});
+
+module.exports = router;
